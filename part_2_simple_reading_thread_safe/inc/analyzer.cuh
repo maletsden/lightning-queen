36f14bc5e9f6e84b109b40432fceddaa505854fa
Denys Maletskyi <maletsden@gmail.com>
2021-06-29 00:07:04 +0300

    feat: wrote part 2 (simple reading with thread safe approach)
    Change List:
    - added for each thread separate space to write results

---
+++ part_2_simple_reading_thread_safe/inc/analyzer.cuh
@@ -0,0 +1,38 @@
+#ifndef PART_1_SIMPLE_READING_THREAD_SAFE_ANALYZER_CUH
+#define PART_1_SIMPLE_READING_THREAD_SAFE_ANALYZER_CUH
+
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <array>
+#include <numeric>
+#include <thread>
+#include <fstream>
+#include <cstdio>
+#include <cassert>
+#include <cuda.h>
+#include <driver_types.h>
+
+#include "ThreadSafeQueue.h"
+#include "helpers.h"
+
+constexpr std::uint32_t CACHE_LINE_SIZE = 128;
+
+
+// Convenience function for checking CUDA runtime API results
+// can be wrapped around any runtime API call. No-op in release builds.
+inline
+cudaError_t checkCuda(cudaError_t result) {
+#if defined(DEBUG) || defined(_DEBUG)
+  if (result != cudaSuccess) {
+    fprintf(stderr, "CUDA Runtime Error: %s\n",
+            cudaGetErrorString(result));
+    assert(result == cudaSuccess);
+  }
+#endif
+  return result;
+}
+
+void run_analyzer(ThreadSafeQueue<std::string>& genomes_queue);
+
+#endif //PART_1_SIMPLE_READING_THREAD_SAFE_ANALYZER_CUH
